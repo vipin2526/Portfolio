b91d29e72fda203c074fd2969f0cdd200757ef7b
vipin kumar <xmartcoder@gmail.com>
2024-02-11 22:47:01 +0530

    readme and some imgages

--- src/Components/ProjectBox.js
+++ src/Components/ProjectBox.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import LinkLogo from './LinkLogo'
+
 
 export default function ProjectBox({ project }) {
     return (
         <div className="container col-md-6 smallProjectBox mb-5">
 
-            <div className="videoBox m-auto overflow-hidden ">
-                {/* <img src="/agra.jpg" alt="" /> */}
+            <div className="imageBox m-auto overflow-hidden"
+                onClick={() => { if (project.live) window.open(project.live) }}>
+                <img src={project.imagepath} alt="" className='h-100' />
             </div>
+
             <ul className='heading fs-5 m-0'>
-                <li className="headingTital">{project.name}</li>
+                <li className="headingTital float-end">{project.name}</li>
+                <li className="headingIcon">
+                    <LinkLogo
+                        to={project.code}
+                        logoPath={"asset/code.svg"}
+                        height={20}
+                        alt={'code'}
+                    />
+                </li>
             </ul>
 
             <div className="discription text-center">
